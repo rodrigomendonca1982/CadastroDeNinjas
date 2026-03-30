5c45fe03a6c9acecbea7c0bdcc1158ebe262b307
unknown <DESENVOLVEDOR@M-0663.matcfn.ad>
2026-03-30 15:06:02 -0300

    Primeiro Commit do CadastroDeNinjas

---
+++ src/test/java/mb/cmatfn/epsd/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.cmatfn.epsd.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
